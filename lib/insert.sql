a00a43b8b1f6ae6aca2f932f4dc22bcd2e74e9e9
Jacob Harrison Lyon <jacobhlyon@Jacobs-MacBook-Air.local>
2017-07-03 11:49:19 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+	VALUES ("Mr. Chocolate", 12, "M", "dark brown", "Softee", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Rowdy", 10, "F", "Grey", "Nice", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Tabitha", 6, "F", "While", "Fierce", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Sergent Brown", 5, "M", "dark brown", "Mean", "false");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Melissa", 13, "F", "Black", "Mean", "false");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Grinch", 2, "M", "White", "Mean", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+	VALUES ("Wendy", 6, "F", "dark brown", "Nice", "true");
+
+INSERT INTO bears (age, gender, color, temperament, alive)
+	VALUES (20, "M", "black", "aggressive", "false");
